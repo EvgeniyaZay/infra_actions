61ac830fd0e460feabf3ff941414fa60bf5fce3b
EvgeniyaZay <zhenyayaya121996@gmail.com>
2023-01-16 21:28:23 +0300

    add test2 with flake8

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 def index(request):
     data = 'У меня получилось!'
-    return HttpResponse(data, status=200)
+    return HttpResponse(data, status_code=200)
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница!', status=200)
+    return HttpResponse('А это вторая страница', status_code=200)
